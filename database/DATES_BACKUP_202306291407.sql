3e364318f69b93f4503d55c5db5eaa917fbe431c
Juan Falco <juan.falco21@gmail.com>
2023-06-29 15:07:03 -0300

    Se arreglo problemas con bd y se agregaron archivos del mismo. Se agrego una nueva version del front que esta en proceso

---
+++ database/DATES_BACKUP_202306291407.sql
@@ -0,0 +1,3 @@
+INSERT INTO DATE_STORAGE.dbo.DATES_BACKUP (id,[date],description,modify_day) VALUES
+	 (1,'2037-06-12',N'Fecha AGREGADA por test',N'29-06-2023'),
+	 (3,'2034-04-09',N'Primera fecha de testing',N'29-06-2023');
---
+++ database/USERS_BACKUP_202306291407.sql
@@ -0,0 +1,2 @@
+INSERT INTO DATE_STORAGE.dbo.USERS_BACKUP (id,email,password,modify_date) VALUES
+	 (1,N'juan.falco@gmail.com',N'$2a$10$fUooRa2Hi9ggrci5dJaLiOht3z6oogTepFD8/icJvz4mNMmmzg3IK',N'29-06-2023');
